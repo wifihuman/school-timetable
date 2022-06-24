ecd91c48e466d0e06161ac2c955080a581881d13
junwoo <96633366+wifihuman@users.noreply.github.com>
2022-06-24 23:04:01 +0900

    Add files via upload

---
+++ script2 (2).js
@@ -0,0 +1,12 @@
+function randomize() {
+    document.getElementById('gift').style.color = randomColors();
+  }
+  
+  
+  // random colors - taken from here:
+  // http://www.paulirish.com/2009/random-hex-color-code-snippets/
+
+
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
